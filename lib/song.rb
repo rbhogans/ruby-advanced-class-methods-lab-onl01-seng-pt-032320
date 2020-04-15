3abce0ce37b052393498a8bb6572b90960b4e7e1
Rabiah Hogans <hogansrabiah@gmail.com>
2020-04-15 05:01:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,42 @@ class Song
   def self.all
     @@all
   end
+  
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  
+end
 
+ def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+    def self.create_by_name(name)
+    song = self.new
+    self.all << song
+      song.name = name
+    song
+  end
+  
+  def self.find_by_name(name)
+
+@@all.find{|song| song.name == name}
+ end
+    result = self.find_by_name(title)
+    if result
+      result
+    else
+      self.create_by_name(title)
+    end
+    
   def save
     self.class.all << self
   end
-
+  
+   
+   
 end
