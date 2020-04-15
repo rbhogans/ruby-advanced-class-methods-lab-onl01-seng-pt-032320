87db459417f87a32fe801589b083f4d5bf97719a
Rabiah Hogans <hogansrabiah@gmail.com>
2020-04-15 05:04:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ end
     
     
 def self.create_from_filename(filename)
-   result = self.create_from_filename(filename)
+   result = self.new_from_filename(filename)
    song = self.create
    song.name = result.name
    song.artist_name = result.artist_name
