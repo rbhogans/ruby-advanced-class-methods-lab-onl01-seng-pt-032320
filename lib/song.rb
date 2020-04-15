e859db5ccb4102e8ebfeb8d0bb4a429740b11894
Rabiah Hogans <hogansrabiah@gmail.com>
2020-04-15 05:15:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,40 +5,37 @@ class Song
   def self.all
     @@all
   end
-  
-  def self.create
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create #class constructor
     song = self.new
     self.all << song
     song
-  
-end
+  end
 
- def self.new_by_name(name)
+  def self.new_by_name(title) #class constructor
     song = self.new
-    song.name = name
+    song.name = title
     song
   end
-  
-    def self.create_by_name(name)
-    song = self.new
-    self.all << song
-      song.name = name
+
+  def self.create_by_name(title) #class constructor
+    song = self.create
+    song.name = title
     song
   end
-  
-  def self.find_by_name(name)
 
-@@all.find{|song| song.name == name}
- end
+  def self.find_by_name(title) #class finder
+    result = self.all.detect {|song| song.name == title}
+    result
+  end
+
+  def self.find_or_create_by_name(title)
+    #either return a matching song instance with that name or create a new song with the name and return the song instance
     result = self.find_by_name(title)
-    if result
-      result
-    else
-      self.create_by_name(title)
-    end
-    
-      def self.find_or_create_by_name(title)
-      result = self.find_by_name(title)
     if result
       result
     else
@@ -59,28 +56,16 @@ end
     song.artist_name = song_array[0]
     song
   end
-  
-  def self.find_or_create_by_name(title)
-    result = self.find_by_name(title)
-    if result
-      result
-    else
-      self.create_by_name(title)
-    end
+
+  def self.create_from_filename(filename)
+    result = self.new_from_filename(filename)
+    song = self.create
+    song.name = result.name
+    song.artist_name = result.artist_name
+    song
   end
 
-  def self.alphabetical
-    sorted = self.all.sort_by {|song| song.name}
-    sorted
+  def self.destroy_all
+    self.all.clear
   end
-
- 
-
-
-  def save
-    self.class.all << self
-  end
-  
-   
-   
 end
